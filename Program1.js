d323ce17bd92f83a0d4932018ef824a376e56f38
Siraj Ahmed <gymsiraj@gmail.com>
2023-10-11 02:28:06 +0500

    sdd

---
+++ Program1.js
@@ -0,0 +1,14 @@
+//Convert all strings first alphabets to uppercase
+let classget = document.querySelector('.class1 ul');
+classget.innerHTML = '';
+let classarr = ['karachi', 'lAhore', 'islAMAbad', 'PeshawaR'];
+
+for(let i = 0; i < classarr.length; i++){
+    let lowerto = classarr[i].toLowerCase();
+    let firstalpha = lowerto.slice(0,1).toUpperCase();
+    lowerto = lowerto.replace(lowerto.slice(0,1), firstalpha);
+    let listitem = document.createElement('li');
+    listitem.textContent = lowerto;
+    classget.appendChild(listitem);
+    
+}
