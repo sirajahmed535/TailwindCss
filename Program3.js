6fb20ad5dbfc11970aaed8ebb392da75a577acd2
Siraj Ahmed <gymsiraj@gmail.com>
2023-10-27 03:32:59 +0500

    fvdd

---
+++ Program3.js
@@ -0,0 +1,7 @@
+//Array Programs
+//Print the string with the help of array
+let cities = ['Karachi', 'Lahore', 'Islamabad', 'Multan', 'Peshawar'];
+
+for(let i = 0; i<cities[0].length; i++){
+    console.log((cities[0])[i]);
+}
