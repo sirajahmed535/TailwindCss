889bbb4e14bb5913671065c008d84ead8aa34d70
Siraj Ahmed <gymsiraj@gmail.com>
2023-10-31 13:21:10 +0500

    sxsx

--- Program1.js
+++ Program1.js
@@ -1,17 +1,31 @@
 //Convert all strings first alphabets to uppercase
 
-let classget = document.querySelector('.class1 ul');
-classget.innerHTML = '';
-let classarr = ['karachi', 'lAhore', 'islAMAbad', 'PeshawaR'];
+// let classget = document.querySelector('.class1 ul');
+// classget.innerHTML = '';
+// let classarr = ['karachi', 'lAhore', 'islAMAbad', 'PeshawaR'];
 
-for(let i = 0; i < classarr.length; i++){
-    let lowerto = classarr[i].toLowerCase();
-    let firstalpha = lowerto.slice(0,1).toUpperCase();
-    lowerto = lowerto.replace(lowerto.slice(0,1), firstalpha);
-    let listitem = document.createElement('li');
-    listitem.textContent = lowerto;
-    classget.appendChild(listitem);
+// for(let i = 0; i < classarr.length; i++){
+//     let lowerto = classarr[i].toLowerCase();
+//     let firstalpha = lowerto.slice(0,1).toUpperCase();
+//     lowerto = lowerto.replace(lowerto.slice(0,1), firstalpha);
+//     let listitem = document.createElement('li');
+//     listitem.textContent = lowerto;
+//     classget.appendChild(listitem);
+    
+// }
+
+
+function btnclick(){
+    const data = new Date();
+    document.querySelector('#innerdiv').innerHTML = data;
+    document.querySelector("#innerdiv").style.display = "block";
+
+}
+
+function hidecontent(){
+    document.querySelector("#innerdiv").style.display = "none";
     
 }
+
 
 
